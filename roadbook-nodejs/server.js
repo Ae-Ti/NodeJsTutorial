f8526c5e5e25ec8a58f0b6c33b56c3675904656a
Ae-Ti <taeilbae7@gmail.com>
2024-05-01 15:49:52 +0900

    add html file

--- roadbook-nodejs/server.js
+++ roadbook-nodejs/server.js
@@ -1,10 +1,17 @@
 const http = require('http');
+const fs = require('fs').promises;
 
-http.createServer((req,res) => {
-    res.writeHead(200,{'Content-Type': 'text/html; charset=utf-8'});
-    res.write('<h1>Node.js server</h1>');
-    res.end('<p> It\'s http module! </p>')
+const server = http.createServer(async (req,res)=>{
+    try {
+        const f = await fs.readFile('./test.html');
+        res.writeHead(200,{'Content-Type': 'text.html; charset=utf-8'});
+        res.end(f);
+    } catch (err) {
+        console.error(error);
+        res.writeHead(500,{'Content-Type': 'text.html; charset=utf-8'});
+        res.end(err.message);
+    }
 })
-    .listen(8080, () => {
-        console.log('8080포트에서 서버 연결 중...')
-    })
+    .listen(8080,() => {
+        console.log('Connecting server from 8080 port...');
+    });
