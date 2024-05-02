f0293bbf46037e26eaf95a9aa1629f0cb9e3a553
Ae-Ti <taeilbae7@gmail.com>
2024-05-03 00:53:32 +0900

    middleware(next(), error handling, static file

--- roadbook-nodejs/express.js
+++ roadbook-nodejs/express.js
@@ -3,9 +3,23 @@ const app = express()
 
 app.set('port', process.env.Port || 8080);
 
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res, next) {
+    res.sendFile(__dirname + '/index.html',() => next())
+});
+
+const myLogger  = function (req, res, next) {
+    console.log('Logged');
+    next();
+};
+
+app.use(myLogger);
+
+app.use(function(err,req,res,next) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!')
 })
+
+app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), () => {
     console.log('Connecting server from', app.get('port'))
